299a7c8d3ceaf1bc35a5fb56f96b4964bf3cdf80
Satyam216 <satyamjain216@gmail.com>
2026-02-11 17:28:29 +0530

    backend file structure ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="bg-red-500 text-white p-4 rounded-lg mt-4">
+        Click on the Vite and React logos to learn more
+      </div>
+  
+  )
+}
+
+export default App
